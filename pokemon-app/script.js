7f54a03cf8503fb680c67f8d9c5ab544593bd8ff
Nicolas <njcaceres04@gmail.com>
2024-08-30 19:43:46 -0500

    added buttons and flowbite

--- pokemon-app/script.js
+++ pokemon-app/script.js
@@ -1,22 +1,20 @@
 async function fetchPokemon() {
     const pokemonName = document.getElementById('pokemonName').value.toLowerCase();
     if (!pokemonName) {
-        alert('Please enter a Pokémon name');
+        alert('Please enter a Pokemon name');
         return;
     }
 
     try {
-        // Endpoint 1: Obtener información básica del Pokémon
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
         if (!response.ok) {
             throw new Error('Pokémon not found');
         }
         const pokemonData = await response.json();
-
-        // Mostrar nombre, peso y foto
         document.getElementById('name').textContent = pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1);
         document.getElementById('weight').textContent = `Weight: ${pokemonData.weight / 10} kg`;
         document.getElementById('photo').src = pokemonData.sprites.front_default;
+        console.log(pokemonData)
 
         // Obtener y mostrar tipos del Pokémon
         const types = pokemonData.types.map(typeInfo => typeInfo.type.name).join(', ');
